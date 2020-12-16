fbaca9c19d93aca75dc3130d7020b28d8c8b255f
Ryan Languay <ryan.languay@mail.mcgill.ca>
2020-12-16 10:16:07 -0500

    Move attention code to new class

--- engine_model/engine.py
+++ engine_model/engine.py
@@ -1,143 +1,13 @@
-from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation, GRU, Conv1D
-from keras.layers import Bidirectional, GlobalMaxPool1D, GlobalMaxPooling1D, GlobalAveragePooling1D
-from keras.layers import Input, Embedding, Dense, Conv2D, MaxPool2D, concatenate
-from keras.layers import Reshape, Flatten, Concatenate, Dropout, SpatialDropout1D
-from keras.optimizers import Adam
-from keras.models import Model
-from keras.engine.topology import Layer
-from keras import initializers, regularizers, constraints, optimizers, layers
-
-
-from keras.layers import *
 from keras.models import *
-from keras import initializers, regularizers, constraints, optimizers, layers
-from keras.initializers import *
-from keras.optimizers import *
-import keras.backend as K
-from keras.callbacks import *
-import tensorflow as tf
-
-####ATTENTION MECHANISM
-def dot_product(x, kernel):
-    """
-    Wrapper for dot product operation, in order to be compatible with both
-    Theano and Tensorflow
-    Args:
-        x (): input
-        kernel (): weights
-    Returns:
-    """
-    if K.backend() == 'tensorflow':
-        return K.squeeze(K.dot(x, K.expand_dims(kernel)), axis=-1)
-    else:
-        return K.dot(x, kernel)
-    
-
-class AttentionWithContext(Layer):
-    """
-    Attention operation, with a context/query vector, for temporal data.
-    Supports Masking.
-    Follows the work of Yang et al. [https://www.cs.cmu.edu/~diyiy/docs/naacl16.pdf]
-    "Hierarchical Attention Networks for Document Classification"
-    by using a context vector to assist the attention
-    # Input shape
-        3D tensor with shape: `(samples, steps, features)`.
-    # Output shape
-        2D tensor with shape: `(samples, features)`.
-    How to use:
-    Just put it on top of an RNN Layer (GRU/LSTM/SimpleRNN) with return_sequences=True.
-    The dimensions are inferred based on the output shape of the RNN.
-    Note: The layer has been tested with Keras 2.0.6
-    Example:
-        model.add(LSTM(64, return_sequences=True))
-        model.add(AttentionWithContext())
-        # next add a Dense layer (for classification/regression) or whatever...
-    """
-    def __init__(self,
-                 W_regularizer=None, u_regularizer=None, b_regularizer=None,
-                 W_constraint=None, u_constraint=None, b_constraint=None,
-                 bias=True, **kwargs):
-
-        self.supports_masking = True
-        self.init = initializers.get('glorot_uniform')
-
-        self.W_regularizer = regularizers.get(W_regularizer)
-        self.u_regularizer = regularizers.get(u_regularizer)
-        self.b_regularizer = regularizers.get(b_regularizer)
-
-        self.W_constraint = constraints.get(W_constraint)
-        self.u_constraint = constraints.get(u_constraint)
-        self.b_constraint = constraints.get(b_constraint)
-
-        self.bias = bias
-        super(AttentionWithContext, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        assert len(input_shape) == 3
-
-        self.W = self.add_weight(shape=(input_shape[-1], input_shape[-1],),
-                                 initializer=self.init,
-                                 name='{}_W'.format(self.name),
-                                 regularizer=self.W_regularizer,
-                                 constraint=self.W_constraint)
-        if self.bias:
-            self.b = self.add_weight(shape=(input_shape[-1],),
-                                     initializer='zero',
-                                     name='{}_b'.format(self.name),
-                                     regularizer=self.b_regularizer,
-                                     constraint=self.b_constraint)
-
-        self.u = self.add_weight(shape=(input_shape[-1],),
-                                 initializer=self.init,
-                                 name='{}_u'.format(self.name),
-                                 regularizer=self.u_regularizer,
-                                 constraint=self.u_constraint)
-
-        super(AttentionWithContext, self).build(input_shape)
-
-    def compute_mask(self, input, input_mask=None):
-        # do not pass the mask to the next layers
-        return None
-
-    def call(self, x, mask=None):
-        uit = dot_product(x, self.W)
-
-        if self.bias:
-            uit += self.b
-
-        uit = K.tanh(uit)
-        ait = dot_product(uit, self.u)
-
-        a = K.exp(ait)
-
-        # apply mask after the exp. will be re-normalized next
-        if mask is not None:
-            # Cast the mask to floatX to avoid float64 upcasting in theano
-            a *= K.cast(mask, K.floatx())
-
-        # in some cases especially in the early stages of training the sum may be almost zero
-        # and this results in NaN's. A workaround is to add a very small positive number ε to the sum.
-        # a /= K.cast(K.sum(a, axis=1, keepdims=True), K.floatx())
-        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
-
-        a = K.expand_dims(a)
-        weighted_input = x * a
-        return K.sum(weighted_input, axis=1)
-
-    def compute_output_shape(self, input_shape):
-        return input_shape[0], input_shape[-1]
-
-
+from keras import layers
 
 from datetime import datetime
 import numpy as np
 import pandas as pd
 
-pd.set_option('max_colwidth', None)
 import matplotlib.pyplot as plt
 import os
-import nltk
-from nltk import word_tokenize
+import attention
 
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
@@ -151,6 +21,8 @@ from tensorflow.python.keras.layers import Embedding
 from tensorflow.python.keras.models import Model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+pd.set_option('max_colwidth', None)
 
 ORIGINAL_DATA = "original_corpus.npz"
 
@@ -319,12 +191,12 @@ class Classifier():
         x = layers.MaxPooling1D(3, padding='same')(x)
         x = layers.Conv1D(128, 2, activation='relu', padding='same')(x)
         x = layers.GlobalMaxPooling1D()(x)
-		
-		### TO RUN BI direction LSTM replace CONV1D for the following
-		#x = layers.Bidirectional(layers.LSTM(64, return_sequences=True))(embedded_sequences)
-        #x = layers.Bidirectional(layers.LSTM(32, return_sequences=True))(x)
-        #x = AttentionWithContext()(x)
-		
+
+        ### TO RUN BI direction LSTM replace CONV1D for the following
+        # x = layers.Bidirectional(layers.LSTM(64, return_sequences=True))(embedded_sequences)
+        # x = layers.Bidirectional(layers.LSTM(32, return_sequences=True))(x)
+        # x = attention.AttentionWithContext()(x)
+
         if costume_feature_flag:
             features = Input(shape=(np.shape(self.__features)[1],), dtype='float32', name='Features')
             merged = layers.Concatenate()([x, features])
